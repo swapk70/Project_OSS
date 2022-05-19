ebb5fb5620b5989ca33f8872b567c16f7bbce862
swapk70 <swapnil.smile@gmail.com>
2022-05-19 16:52:55 +0530

    Initial changes

---
+++ Addition.py
@@ -0,0 +1,9 @@
+a = 40
+b = 20
+
+def add(a,b):
+    c = a + b
+    return c
+
+ans = add(a,b)
+print(f"Addition is : {ans}")
---
+++ mul.py
@@ -0,0 +1,10 @@
+a = 10
+b = 20
+
+def mul(a,b):
+    c = a * b
+    return  c
+
+ans = mul(a,b)
+
+print("Mul is:",ans)
