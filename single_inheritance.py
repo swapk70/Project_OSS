f50b7d8c1d363bf720fb79a15cc91addd8e0a5c7
swapk70 <swapnil.smile@gmail.com>
2022-06-16 13:57:58 +0530

    Initial Change

---
+++ single_inheritance.py
@@ -0,0 +1,26 @@
+class person:
+    def __init__(self, Name, Age, Address):
+        self.Name = Name
+        self.Age = Age
+        self.Address = Address
+
+    def display(self):
+        print(f"Name: {self.Name}")
+        print(f"Age:  {self.Age}")
+        print(f"Address: {self.Address}")
+
+class employee(person):
+    def __init__(self, Name, Age, Address, eid, dept, salary):
+        super().__init__(Name, Age, Address)
+        self.eid = eid
+        self.dept = dept
+        self.salary = salary
+
+    def display(self):
+        super().display()
+        print(f"Eid: {self.eid}")
+        print(f"Dept: {self.dept}")
+        print(f"Salary: {self.salary}")
+
+e = employee("Swapnil", 30, "Ashta", 3003, "CSE", 40000)
+e.display()
